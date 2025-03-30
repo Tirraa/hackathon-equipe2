12c0fb3289d4670e268d1ec445919820b5ead5fd
ErwinPe <erwinp@neuf.fr>
2025-03-30 22:02:52 +0200

    chore: update projet icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "ZobieLeClimat",
+  description: "ZobieLeClimat",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/ping.png" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
